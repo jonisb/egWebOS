8d521bf120662da7096b7ebed1b935c370358535
jonisb <github.com@JsBComputing.se>
2022-01-17 22:12:47 +0100

    Adds main Config dialog with IP setting

--- egWebOS/__init__.py
+++ egWebOS/__init__.py
@@ -20,7 +20,31 @@ class WebOS(eg.PluginClass):
     def __init__(self):  # TODO:
         pass
 
-    def __start__(self):  # TODO:
+    def Configure(self, IP=''):  # TODO: Separate args or combined?
+        def initPanel(self):
+            sizer_1 = wx.BoxSizer(wx.VERTICAL)
+
+            sizer_2 = wx.BoxSizer(wx.HORIZONTAL)
+            sizer_1.Add(sizer_2, 1, wx.EXPAND, 0)
+
+            IP_label = wx.StaticText(self, wx.ID_ANY, "IP address")
+            sizer_2.Add(IP_label, 0, 0, 0)
+
+            self.IP = wx.TextCtrl(self, wx.ID_ANY, IP)
+            sizer_2.Add(self.IP, 0, 0, 0)
+
+            self.SetSizer(sizer_1)
+            sizer_1.Fit(self)
+
+            self.Layout()
+
+        panel = eg.ConfigPanel()
+        initPanel(panel)
+
+        while panel.Affirmed():
+            panel.SetResult(panel.IP.GetValue())
+
+    def __start__(self, IP):  # TODO: Match Configure
         pass
 
     def __stop__(self):  # TODO:
