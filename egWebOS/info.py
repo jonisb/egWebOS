0e5b898cc3870d841347446f419a220211c49ef7
jonisb <github.com@JsBComputing.se>
2022-01-28 05:26:43 +0100

    Release testing

--- egWebOS/info.py
+++ egWebOS/info.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 name = u"WebOS connect"
 author = u"Joni Borén"
-version = "0.0.8"
+version = "0.0.9"
 guid = "{523fa3a6-f1a9-405d-a28a-8d211f76562b}"
 url = u"https://github.com/jonisb/egWebOS/issues"  # TODO: Create support thread and link.
 description = u"""Adds actions to control WebOS devices like LG TVs."""  # TODO: Add description, use <rst>?
